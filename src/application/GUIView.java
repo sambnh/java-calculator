454c82b72aedb2c143fcccac52d81dfecd67cc88
Sam Hollow <zeac114@live.rhul.ac.uk>
2018-11-29 16:49:27 +0000

    Refactored GUIView.fxml and Main.java, added fxml controls as variables in GUIView.java and added interface View.java which is implemented by GUIView.java

--- src/application/GUIView.java
+++ src/application/GUIView.java
@@ -1,11 +1,34 @@
 package application;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 
-public class GUIView {
- 
+public class GUIView implements View{
+    
   @FXML
-  private ToggleGroup radio;
+  void calculateButtonPressed() {
+    answerText.setText("1");
+  }
 
+  @FXML // fx:id="answerText"
+  private TextField answerText; // Value injected by FXMLLoader
+
+  @FXML // fx:id="reverseRadio"
+  private RadioButton reverseRadio; // Value injected by FXMLLoader
+
+  @FXML // fx:id="infixRadio"
+  private RadioButton infixRadio; // Value injected by FXMLLoader
+
+  @FXML // fx:id="calculateButton"
+  private Button calculateButton; // Value injected by FXMLLoader
+
+  @FXML // fx:id="type"
+  private ToggleGroup type; // Value injected by FXMLLoader
+
+  @FXML // fx:id="expressionText"
+  private TextField expressionText; // Value injected by FXMLLoader
+  
 }
